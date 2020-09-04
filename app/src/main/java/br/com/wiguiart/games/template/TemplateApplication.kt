a125711e11104398e561e5cd926d9ae877cd658e
William José Fernandes <williamjosefernandes@gmail.com>
2020-09-04 12:40:21 -0300

    Configurações do Firebase

--- app/src/main/java/br/com/wiguiart/games/template/TemplateApplication.kt
+++ app/src/main/java/br/com/wiguiart/games/template/TemplateApplication.kt
@@ -3,14 +3,41 @@ package br.com.wiguiart.games.template
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
+/**
+ * Classe correspondente ao objeto de aplicação do app
+ *
+ * Essa classe é uma versão customizada do objeto de aplicação (Application) do app.
+ * Ela pode armazenar valores e variáveis de uso comum, uma vez que o ciclo de vida desse objeto
+ * corresponde quase que integralmente ao ciclo de vida do App
+ *
+ * @since 1.0
+ */
 class TemplateApplication : Application() {
+
+    lateinit var firebaseAnalytics: FirebaseAnalytics
+        private set
+
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         MultiDex.install(this)
     }
 
+    /**
+     * Método onCreate do ciclo de vida da Application
+     *
+     * Método que é parte do ciclo de vida do Android. Executado apenas uma vez durante o
+     * ciclo de vida do app, é ideal para iniciar objetos de uso generalizado no app
+     *
+     * @since 1.0
+     */
     override fun onCreate() {
         super.onCreate()
+
+        firebaseAnalytics = Firebase.analytics
     }
+
 }
